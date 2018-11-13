1fd5f6d1718cb36f5db5c7c9defb7f269e5fdbf1
drood <dbernstein1987@hotmail.com>
2018-11-13 16:10:29 +1100

    added play on mouseclick feature

--- script.js
+++ script.js
@@ -15,5 +15,18 @@ function removeTransition(e) {
     this.classList.remove("playing");
 }
 
+function playOnMouseClick() {
+    const getKeyNumber = this.getAttribute("data-key");
+    const key = document.querySelector(`.key[data-key="${getKeyNumber}"]`);
+    const audio = document.querySelector(`audio[data-key="${getKeyNumber}"]`);
+    if (!audio) return; // stop the function from executing
+    audio.currentTime = 0; // rewind to the start
+    console.log(getKeyNumber);
+    audio.play();
+    key.classList.add("playing");
+}
+
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
+keys.forEach(key => key.addEventListener("click", playOnMouseClick));
+// keys.addEventListener("click", playSound);
